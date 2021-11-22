2ccb619e01b79893f28bce498c47eab671256c46
steffilazerte <steffi@steffi.ca>
2021-11-22 12:09:31 -0600

    Add details

---
+++ universe_files.R
@@ -0,0 +1,16 @@
+library(dplyr)
+
+df <- tribble(
+  ~package,                  ~url,
+  "weathercan",              "https://github.com/ropensci/weathercan",
+  "LITAP",                   "https://github.com/FRDC-SHL/LITAP",
+  "cavityuse",               "https://github.com/steffilazerte/cavityuse",
+  "feedr",                   "https://github.com/animalnexus/feedr",
+  "motus",                   "https://github.com/MotusWTS/motus",
+  "naturecounts",            "https://github.com/BirdStudiesCanada/naturecounts",
+  "rems2aquachem",           "https://github.com/bcgov/rems2aquachem",
+  "aquifer-factsheets",      "https://github.com/bcgov/aquifer-factsheets",
+  "bcgwlreports",            "https://github.com/bcgov/bcgwlreports",
+  "moosecounter",            "https://github.com/psolymos/moosecounter") %>%
+  mutate(subdir = NA)
+jsonlite::write_json(df, 'packages.json', pretty = TRUE)
