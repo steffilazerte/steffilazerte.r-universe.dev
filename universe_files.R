915c7a58cc379c3719a144781dd82e20ae2b959e
steffilazerte <steffi@steffi.ca>
2022-02-01 09:03:43 -0600

    Add fasstrshiny

--- universe_files.R
+++ universe_files.R
@@ -10,9 +10,10 @@ df <- tribble(
   "motus",                   "https://github.com/MotusWTS/motus",
   "naturecounts",            "https://github.com/BirdStudiesCanada/naturecounts",
   "rems2aquachem",           "https://github.com/bcgov/rems2aquachem",
-  #"aquifer-factsheets",      "https://github.com/bcgov/aquifer-factsheets",
+# "aquifer-factsheets",      "https://github.com/bcgov/aquifer-factsheets",
   "bcgwlreports",            "https://github.com/bcgov/bcgwlreports",
   "bcaquiferdata",           "https://github.com/steffilazerte/bcaquiferdata",
-  "moosecounter",            "https://github.com/psolymos/moosecounter")
+  "moosecounter",            "https://github.com/psolymos/moosecounter",
+  "fasstrshiny",             "https://github.com/bcgov/fasstrshiny")
 
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
